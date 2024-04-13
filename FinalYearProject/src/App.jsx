76d0dca85a83afaa85bde5239ccbb5ab132d1b4c
Mahadev <129663057+mahadevhere@users.noreply.github.com>
2024-04-13 20:36:30 +0000

    Done

--- FinalYearProject/src/App.jsx
+++ FinalYearProject/src/App.jsx
@@ -2,9 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Admin from "./pages/Admin";
 import Blog from "./pages/Blog";
+import BlogContext from "./context/BlogContext";
+import { useState } from "react";
 
 export default function App() {
+
+  const[isAuth,setIsAuth] = useState(false)
+
   return (
+    <BlogContext.Provider value={{isAuth,setIsAuth}} > 
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -12,5 +18,6 @@ export default function App() {
         <Route path="/blog/:id" element={<Blog />} />
       </Routes>
     </BrowserRouter>
+    </BlogContext.Provider>
   );
 }
--- FinalYearProject/src/pages/Admin.jsx
+++ FinalYearProject/src/pages/Admin.jsx
@@ -1,13 +1,53 @@
-import React from 'react'
+import { useContext } from "react";
+import BlogContext from "../context/BlogContext";
+import AdminNavbar from "../components/AdminNavbar";
 
 const Admin = () => {
+
+const {isAuth, SetIsAuth} = useContext(BlogContext);
+
+
   return (
     <div>
-      <div>
-        <form action=""></form>
-      </div>
-    </div>
-  )
-}
 
-export default Admin
+      {
+        !isAuth?  (      
+        <div className="h-screen flex justify-center items-center">
+        <form action="" className='grid grid-cols-1 gap-3 bg-white w-[80vw] md:w-[20vw] p-3 rounded-lg'>
+
+          <div className='flex flex-col'>
+            <label htmlFor="username" className='text-lg font-semibold text-gray-600'>Username</label>
+            <input type="text" name='username' id='user' 
+            placeholder='Enter your username ' 
+            required 
+            className='rounded-2xl px-3 py-1 text-lg outline-none bg-gray-100'
+            />
+          </div>
+          <div className='flex flex-col'>
+            <label htmlFor="password" 
+            className='text-lg font-semibold text-gray-600'>
+              Password
+            </label>
+            <input type="password" name='password' id='password' 
+            placeholder='Enter your Password ' 
+            required 
+            className='rounded-2xl px-3 py-1 text-lg outline-none bg-gray-100'
+            />
+          </div>
+          <button 
+          type='submit' className='bg-purple-500 rounded-2xl px-3 py-1 text-gray-100 font-semibold hover: 
+          bg-purple-600 transition-all duration-300 ease-in-out'>Login</button>
+        </form>
+      </div> ): (
+      <div>
+          <AdminNavbar/>
+        </div>
+        
+      )}
+
+
+    </div>
+  );
+};
+
+export default Admin;
